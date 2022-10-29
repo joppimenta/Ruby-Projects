b386e8815af359cafc6d6496a7c988247203c316
joppimenta <joaopedropimenta@alu.ufc.br>
2022-10-29 14:09:01 -0300

    first project

---
+++ bubblesort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(v)
+    for i in (0..v.size()-1)
+        for j in (0..v.size()-2)
+            if v[j] > v[j+1]
+                temp = v[j]
+                v[j] = v[j+1]
+                v[j+1] = temp
+            end
+        end
+    end
+
+    puts v
+
+end
+
+bubble_sort([4,3,78,2,0,2])
+
+
+
+
+
+
